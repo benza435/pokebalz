b75d512230d44bb5f9735e1b411c9b035157a52c
ben <benegan435@gmail.com>
2021-07-06 20:12:53 +0100

    extracted fetch function

--- App.js
+++ App.js
@@ -1,65 +1,38 @@
-import React, {useEffect, useState} from "react";
-import {styles} from './AppStyle'
-import { View, Text, Image, TouchableOpacity, TouchableHighlight } from "react-native";
-import Name from './components/Name'
+import React, { useEffect, useState } from 'react';
+import { styles } from './AppStyle';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+import getRandomPokemon from './mvc/gets';
 
 export default function App() {
+	const [pokemon, setPokemon] = useState([]);
+	const [pokeImage, setPokeImage] = useState(['https://picsum.photos/200']);
+	const [showName, setShowName] = useState('Tap the Pokemon to see its name!');
 
-  const [pokemon, setPokemon] = useState([]);
-  const [pokeImage, setPokeImage] = useState(['https://picsum.photos/200'])
-  const [showName, setShowName] = useState("Tap the Pokemon to see its name!")
+	useEffect(() => {
+		getRandomPokemon();
+	}, []);
 
-  const getRandomPokemon = () => {
-    let random = Math.floor(Math.random()*899)
-    const url = `https://pokeapi.co/api/v2/pokemon/${random}`
-    console.log(`### getting pokemon from ${url}`) 
-    return fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-       setPokemon(data.name)
-       setPokeImage(data.sprites.other["official-artwork"].front_default),
-       setShowName("Tap the Pokemon to see its name!")
-       console.log(`showName: ${showName}`)
-       return data.name
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  };
+	return (
+		<View style={styles.main}>
+			<Text>Pok-e-balz</Text>
+			<Text style={styles.name}>{showName}!</Text>
+			<TouchableOpacity
+				onPress={() => {
+					setShowName(`This is ${pokemon}`);
+				}}
+			>
+				<Image style={styles.pokeImage} source={{ uri: `${pokeImage}` }} />
+			</TouchableOpacity>
 
-
-  useEffect(() => {
-    getRandomPokemon()
-  }, []);
-
-
-  return (
-    <View
-      style={styles.main}
-    >
-      <Text>Pok-e-balz</Text>
-
-      {/* <Name props={pokemon, showName}/> */}
-      <Text style={styles.name}>{showName}!</Text>
-        <TouchableOpacity onPress={()=>{
-          setShowName(`This is ${pokemon}`)
-          console.log(`show: ${showName}`)}}>
-          <Image style={styles.pokeImage} source={{uri: `${pokeImage}`}} 
-          />
-        </TouchableOpacity>
-        
-            <Text style={styles.name}>Tap the Pokeball to get a new Pokemon!</Text>
-        <View style={{height: '20%'}}>
-
-          <TouchableOpacity style={styles.pokeball} onPress={getRandomPokemon}>
-            <Image style={styles.pokeball} source={require('./img/pokeball.png')}/>
-          </TouchableOpacity>
-
-        </View>
-    </View>
-  );
+			<Text style={styles.name}>Tap the Pokeball to get a new Pokemon!</Text>
+			<View style={{ height: '20%' }}>
+				<TouchableOpacity style={styles.pokeball} onPress={getRandomPokemon}>
+					<Image
+						style={styles.pokeball}
+						source={require('./img/pokeball.png')}
+					/>
+				</TouchableOpacity>
+			</View>
+		</View>
+	);
 }
-
-
-
-
--- mvc/gets.js
+++ mvc/gets.js
@@ -1,14 +1,17 @@
-export const getRandomPokemon = () => {
-    const random = Math.floor(Math.random()*899)
-    const url = `https://pokeapi.co/api/v2/pokemon/${random}`
-    console.log(`getting pokemon from ${url}`) 
-    return fetch(url)
-      .then((response) => response.json())
-      .then((pokemon) => {
-          console.log(pokemon.name)
-        return pokemon.name;
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  };
+const getRandomPokemon = () => {
+	let random = Math.floor(Math.random() * 899);
+	const url = `https://pokeapi.co/api/v2/pokemon/${random}`;
+	console.log(`### getting pokemon from ${url}`);
+	return fetch(url)
+		.then((response) => response.json())
+		.then((data) => {
+			setPokemon(data.name);
+			setPokeImage(data.sprites.other['official-artwork'].front_default),
+				setShowName('Tap the Pokemon to see its name!');
+			console.log(`showName: ${showName}`);
+			return data.name;
+		})
+		.catch((error) => {
+			console.error(error);
+		});
+};
