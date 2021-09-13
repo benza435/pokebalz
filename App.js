5cdb5a340ea16076f4dd37263f3f4512ea9c6f44
unknown <benegan435@gmail.com>
2021-09-13 11:44:19 +0100

    reintegrated functions into single file

--- App.js
+++ App.js
@@ -1,38 +1,57 @@
-import React, { useEffect, useState } from 'react';
-import { styles } from './AppStyle';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
-import getRandomPokemon from './mvc/gets';
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { styles } from "./AppStyle";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 
 export default function App() {
-	const [pokemon, setPokemon] = useState([]);
-	const [pokeImage, setPokeImage] = useState(['https://picsum.photos/200']);
-	const [showName, setShowName] = useState('Tap the Pokemon to see its name!');
+  const [pokemon, setPokemon] = useState([]);
+  const [pokeImage, setPokeImage] = useState(["https://picsum.photos/200"]);
+  const [showName, setShowName] = useState("Tap the Pokemon to see its name!");
 
-	useEffect(() => {
-		getRandomPokemon();
-	}, []);
+  const getRandomPokemon = () => {
+    let random = Math.floor(Math.random() * 899);
+    const url = `https://pokeapi.co/api/v2/pokemon/${random}`;
+    console.log(`### getting pokemon from ${url}`);
+    return fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setPokemon(data.name);
+        setPokeImage(data.sprites.other["official-artwork"].front_default),
+          setShowName("Tap the Pokemon to see its name!");
+        console.log(`showName: ${showName}`);
+        return data.name;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
-	return (
-		<View style={styles.main}>
-			<Text>Pok-e-balz</Text>
-			<Text style={styles.name}>{showName}!</Text>
-			<TouchableOpacity
-				onPress={() => {
-					setShowName(`This is ${pokemon}`);
-				}}
-			>
-				<Image style={styles.pokeImage} source={{ uri: `${pokeImage}` }} />
-			</TouchableOpacity>
+  useEffect(() => {
+    getRandomPokemon();
+  }, []);
 
-			<Text style={styles.name}>Tap the Pokeball to get a new Pokemon!</Text>
-			<View style={{ height: '20%' }}>
-				<TouchableOpacity style={styles.pokeball} onPress={getRandomPokemon}>
-					<Image
-						style={styles.pokeball}
-						source={require('./img/pokeball.png')}
-					/>
-				</TouchableOpacity>
-			</View>
-		</View>
-	);
+  return (
+    <View style={styles.main}>
+      <Text>Pok-e-balz</Text>
+      <Text style={styles.name}>{showName}!</Text>
+      <TouchableOpacity
+        onPress={() => {
+          setShowName(`This is ${pokemon}`);
+        }}
+      >
+        <Image style={styles.pokeImage} source={{ uri: `${pokeImage}` }} />
+      </TouchableOpacity>
+
+      <Text style={styles.name}>Tap the Pokeball to get a new Pokemon!</Text>
+      <View style={{ height: "20%" }}>
+        <TouchableOpacity style={styles.pokeball} onPress={getRandomPokemon}>
+          <Image
+            style={styles.pokeball}
+            source={require("./img/pokeball.png")}
+          />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
